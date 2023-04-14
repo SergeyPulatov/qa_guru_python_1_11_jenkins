1509f4b0a8d4812c3ccc594b42447939d2d49571
sergey.pulatov <sergey.pulatov@gs-labs.ru>
2023-04-14 21:21:07 +0300

    add browser selene test

--- tests/demoqa/test_registration.py
+++ tests/demoqa/test_registration.py
@@ -5,7 +5,7 @@ from selene import browser, have, command
 
 from util import attach
 
-
+@allure.title('Suc')
 def test_browser_submit():
     with allure.step('Open URL'):
         browser.open_url('https://demoqa.com/automation-practice-form')
@@ -24,7 +24,7 @@ def test_browser_submit():
         browser.element('#subjectsInput').type('Economics').press_enter()
         browser.element('[for = "hobbies-checkbox-1"]').click()
         browser.element('[for = "hobbies-checkbox-3"]').click()
-        browser.element('#uploadPicture').send_keys(os.getcwd() + "/image.png")
+        browser.element('#uploadPicture').send_keys(os.getcwd() + "/tests/demoqa/image2.png")
         browser.element('#currentAddress').type('Saint-Petersburg, Lensoveta street, 53')
         browser.execute_script("window.scrollBy(0, 500)")
         browser.element('#react-select-3-input').type('NCR').press_enter()
@@ -40,7 +40,7 @@ def test_browser_submit():
         browser.element('//table/tbody/tr[5]/td[2]').should(have.text('02 May,1997'))
         browser.element('//table/tbody/tr[6]/td[2]').should(have.text('Economics'))
         browser.element('//table/tbody/tr[7]/td[2]').should(have.text('Sports, Music'))
-        browser.element('//table/tbody/tr[8]/td[2]').should(have.text('image.png'))
+        browser.element('//table/tbody/tr[8]/td[2]').should(have.text('image2.png'))
         browser.element('//table/tbody/tr[9]/td[2]').should(have.text('Saint-Petersburg, Lensoveta street, 53'))
         browser.element('//table/tbody/tr[10]/td[2]').should(have.text('NCR Gurgaon'))
 
